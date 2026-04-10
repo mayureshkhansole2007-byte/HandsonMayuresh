3cb6b7a86cddbb700e38378eedd84f9ffcc9307e
Your Name <you@example.com>
2026-04-10 11:27:14 +0530

    Add GitHub Pages deployment workflow and update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/handson/", // Replace 'handson' with your GitHub repository name
 })
